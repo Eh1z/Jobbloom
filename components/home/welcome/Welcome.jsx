28fa81ab44de0bfadcc1502cbccda5cf81eb758e
Eh1z <godsentehiz11@gmail.com>
2023-05-28 23:31:46 +0100

    Welcome section almost done

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState} from 'react'
+import { View, 
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList
+ } from 'react-native';
 
-import styles from './welcome.style'
+import { useRouter } from "expo-router";
+import styles from './welcome.style';
+import { icons, SIZES } from "../../../constants";
 
 const Welcome = () => {
+  const router = useRouter();
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Godsent</Text>
+        <Text style={styles.welcomeMessage}>Find Your Dream Job</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder="What are you looking for?"
+          />
+        </View>
+      </View>
     </View>
   )
 }
--- constants/images.js
+++ constants/images.js
@@ -1,4 +1,4 @@
-import profile from "../assets/images/kemal.jpg";
+import profile from "../assets/images/dp.jpg";
 
 export default {
   profile,
