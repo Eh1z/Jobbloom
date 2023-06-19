5ff43f2c74676807b216216146de945bd7f30cb1
Eh1z <godsentehiz11@gmail.com>
2023-06-20 00:40:32 +0100

    working on job details page

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useCallback, useState } from "react";
+import { 
+    TextInputTextInputEventData, View, SafeAreaView, ScrollView, ActivityIndicator, RefreshControl
+ } from "react-native";
+import { Stack, useRouter, useSearchParams } from "expo-router";
+
+import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } from "../../components";
+import { COLORS, icons, images, SIZES } from "../../constants";
+import useFetch from "../../hook/useFetch"
+
 
 const JobDetails = () => {
+
+    
   return (
     <div>JobDetails</div>
   )
