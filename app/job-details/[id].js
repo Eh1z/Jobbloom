e5d8cd652a8994333db970e453aab5243351f0d9
Eh1z <godsentehiz11@gmail.com>
2023-06-20 01:00:48 +0100

    going to bed for now, continue in the morning

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -10,10 +10,38 @@ import useFetch from "../../hook/useFetch"
 
 
 const JobDetails = () => {
+    const params = useSearchParams();
+    const router = useRouter();
 
-    
+    const { data, isLoading, error, refetch } = useFetch('job-details', { job_id: params.id })
+
   return (
-    <div>JobDetails</div>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+        <Stack.Screen
+        options={{headerStyle: { backgroundColor: COLORS.lightWhite },
+        headerShadowVisible: false,
+        headerBackVisible: false,
+        headerLeft: () => (
+            <ScreenHeaderBtn 
+            iconUrl={icons.left} 
+            dimension="60%" 
+            onPress={() => router.back()}/>
+        ),
+        headerRight: () => (
+            <ScreenHeaderBtn 
+            iconUrl={icons.share} 
+            dimension="60%" 
+            />
+        ),
+        headerTitle: "",
+    }}/>
+
+    <>
+    <ScrollView showsVerticalScrollIndicator={false} 
+    refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>}>
+    </ScrollView>
+    </>
+    </SafeAreaView>
   )
 }
 
