2962b0f951f3112123b730e3ebe846639b2ffbdf
Eh1z <godsentehiz11@gmail.com>
2023-10-14 22:33:11 +0100

    added a job details page

--- Jobbloom App/components/jobdetails/company/Company.jsx
+++ Jobbloom App/components/jobdetails/company/Company.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import { View, Text, Image } from 'react-native'
+import React from "react";
+import { View, Text, Image } from "react-native";
 
-import styles from './company.style'
-import { icons } from '../../../constants'
-import { checkImageURL } from '../../../checkers'
+import styles from "./company.style";
+import { icons } from "../../../constants";
+import { checkImageURL } from "../../../utils";
 
 const Company = ({ companyLogo, jobTitle, companyName, location }) => {
   return (
     <View style={styles.container}>
-
       <View style={styles.logoBox}>
         <Image
-        source={{
-          uri: checkImageURL(companyLogo)
-          ? companyLogo
-          : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg"
-        }}
-        style={styles.logoImage}
+          source={{
+            uri: checkImageURL(companyLogo)
+              ? companyLogo
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+          }}
+          style={styles.logoImage}
         />
       </View>
 
@@ -25,19 +24,18 @@ const Company = ({ companyLogo, jobTitle, companyName, location }) => {
       </View>
 
       <View style={styles.companyInfoBox}>
-        <Text style={styles.companyName}>{companyName}</Text>
+        <Text style={styles.companyName}>{companyName} / </Text>
         <View style={styles.locationBox}>
           <Image
-          source={icons.location}
-          resizeMode="contain"
-          style={styles.locationImage}
+            source={icons.location}
+            resizeMode='contain'
+            style={styles.locationImage}
           />
           <Text style={styles.locationName}>{location}</Text>
         </View>
       </View>
-      
     </View>
-  )
-}
+  );
+};
 
-export default Company
+export default Company;
